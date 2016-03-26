144b94a31285bccb4b01ff074aa88dd42204982c
Ralph Kristofelle A. Santiago <rappsantiago28@gmail.com>
2016-03-27 01:18:44 +0800

    added initial implementation for view history

--- app/src/main/java/com/rappsantiago/weighttracker/HistoryFragment.java
+++ app/src/main/java/com/rappsantiago/weighttracker/HistoryFragment.java
@@ -16,20 +16,73 @@
 
 package com.rappsantiago.weighttracker;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import static com.rappsantiago.weighttracker.provider.WeightTrackerContract.*;
 
 /**
  * Created by rappsantiago28 on 3/26/16.
  */
-public class HistoryFragment extends Fragment {
+public class HistoryFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String TAG = HistoryFragment.class.getSimpleName();
+
+    private ListView mListView;
+
+    private SimpleCursorAdapter mCursorAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        mListView = (ListView) view;
+
+        mCursorAdapter = new SimpleCursorAdapter(getContext(),
+            android.R.layout.simple_list_item_2, null,
+            new String[]{Progress.COL_NEW_WEIGHT, Progress.COL_TIMESTAMP},
+            new int[]{android.R.id.text1, android.R.id.text2}, 0);
+
+        mListView.setAdapter(mCursorAdapter);
+
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLoaderManager().restartLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(getContext(), Progress.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.d(TAG, "count = " + data.getCount());
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
